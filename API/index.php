a68581ee790cd8389966415681649769a5f23a09
Owen Rempel <owenrempel@gmail.com>
2023-07-29 10:17:38 -0600

    more testing

--- API/index.php
+++ API/index.php
@@ -370,7 +370,8 @@ function getFormStruct($formArray, $redirectName, $action){
     $arrayToSend = [];
     $arrayToSend['form']['formName'] = $formArray['formName'];
     $arrayToSend['form']['formTitle'] = 'Add '.$formArray['formTitle'];
-    $arrayToSend['form']['callBack'] = ($_ENV['HTTPS'] ? "https" : 'http').'://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].'/API/'.$redirectName;
+    $arrayToSend['form']['callBack'] = '/API/'.$redirectName;
+    //($_ENV['HTTPS'] ? "https" : 'http').'://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].
     foreach($formArray['items'] as $items){
        
         $itemArray = [];
