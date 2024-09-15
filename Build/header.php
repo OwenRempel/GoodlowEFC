7226e2b3bb2ec0e71c04c6e0e0fa8a5058d59fb2
Owen Rempel <owenrempel@gmail.com>
2024-09-14 20:25:18 -0500

    Running some more tweaks on the site

--- Build/header.php
+++ Build/header.php
@@ -3,11 +3,7 @@
         <img src="/MediaFiles/photos/logo2.png" alt="logo" class="logo"/>
         <div class="sunday">
             <h3>Praise and worship service starts at 11:00 A.M.</h3>
-<<<<<<< HEAD
             <h3>Sunday School for all ages, starts at 9:45 A.M.</h3>
-=======
-            <h3 style="display:none">Sunday School for all ages, starts at 9:45 A.M.</h3>
->>>>>>> 4b3e8d822fe419d8ae01bb7f29fa3f5297c7bcc1
         </div>
       </div>
      <?php
