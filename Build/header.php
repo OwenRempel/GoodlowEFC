0883d93f19dec09b71143b786fca3306b87da39e
Owen Rempel <owenrempel@gmail.com>
2024-09-14 20:19:20 -0500

    Bringing back the anouncment for sunday school

--- Build/header.php
+++ Build/header.php
@@ -3,7 +3,7 @@
         <img src="/MediaFiles/photos/logo2.png" alt="logo" class="logo"/>
         <div class="sunday">
             <h3>Praise and worship service starts at 11:00 A.M.</h3>
-            <h3 style="display:unset">Sunday School for all ages, starts at 9:45 A.M.</h3>
+            <h3>Sunday School for all ages, starts at 9:45 A.M.</h3>
         </div>
       </div>
      <?php
