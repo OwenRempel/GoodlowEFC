85fbc829b2fb3ac814ebbf28957425ed9c07b7b4
Owen Rempel <owenrempel@gmail.com>
2024-11-21 23:20:10 -0700

    Updating the header so it is better designed

--- Build/header.php
+++ Build/header.php
@@ -2,7 +2,7 @@
       <div class="header">
         <img src="/MediaFiles/photos/logo2.png" alt="logo" class="logo"/>
         <div class="sunday">
-            <h3>Worship service starts at 11:00 A.M. Sunday School for all ages, at 9:45 A.M.</h3>
+            <p>Worship service starts at 11:00 A.M. Sunday School for all ages, at 9:45 A.M.</p>
         </div>
       </div>
      <?php
