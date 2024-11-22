a8354fcc16778eeaab6d0aec4870ccee0efab7ba
Owen Rempel <owenrempel@gmail.com>
2024-11-21 23:08:25 -0700

    Changing the part that shows the services

--- Build/header.php
+++ Build/header.php
@@ -2,8 +2,7 @@
       <div class="header">
         <img src="/MediaFiles/photos/logo2.png" alt="logo" class="logo"/>
         <div class="sunday">
-            <h3>Praise and worship service starts at 11:00 A.M.</h3>
-            <h3>Sunday School for all ages, starts at 9:45 A.M.</h3>
+            <h3>Worship service starts at 11:00 A.M. Sunday School for all ages, at 9:45 A.M.</h3>
         </div>
       </div>
      <?php
