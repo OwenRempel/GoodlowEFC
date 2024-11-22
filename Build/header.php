a8f2c118e8e6ae5b405f0aa3ce1eb391e778205b
Owen Rempel <owenrempel@gmail.com>
2024-11-21 23:34:15 -0700

    Testing to see how this works on mobile

--- Build/header.php
+++ Build/header.php
@@ -1,9 +1,9 @@
 <div class='header-wrap'>
-      <div class="header">
-        <img src="/MediaFiles/photos/logo2.png" alt="logo" class="logo"/>
         <div class="sunday">
             <p>Worship service starts at 11:00 A.M. Sunday School for all ages, at 9:45 A.M.</p>
         </div>
+      <div class="header">
+        <img src="/MediaFiles/photos/logo2.png" alt="logo" class="logo"/>
       </div>
      <?php
         include_once('Build/nav.php');
